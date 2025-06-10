d0f43a095a2f8b05eb695dec5ec26ed4365565e4
maruff77 <mhmmdmrf77@gmail.com>
2025-06-10 23:58:05 +0800

    melengkapi method matematika

--- src/main/java/hitung/matematika.java
+++ src/main/java/hitung/matematika.java
@@ -7,10 +7,26 @@ public class matematika {
     //constructor
      public matematika(double bil1, double bil2) {
          this.bil1 = bil1;
-         this.bil2 - bil2;
+         this.bil2 = bil2;
      }
      
+     //setpenjumlahan
      double setPenjumlahan(){
          return bil1 + bil2;
+     }
+     
+     //setpengurangan
+     double setPengurangan(){
+         return bil1 - bil2;
+     }
+     
+     //setperkalian
+     double setPerkalian(){
+         return bil1 * bil2;
+     }
+     
+     //setpembagian
+     double setPembagian(){
+         return bil1 / bil2;
      }
 }
